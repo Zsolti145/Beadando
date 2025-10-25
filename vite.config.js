a8bf9387c744804afa8976c95b2c0190d449891c
Zsolti145 <zsoltika192@gmail.com>
2025-10-25 22:37:00 +0200

    add some plugins to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from "path";
+import { copyFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),{
+
+   name: 'copy-redirects',
+      closeBundle() {
+        copyFileSync(resolve(__dirname, 'public/_redirects'), resolve(__dirname, 'dist/_redirects'));
+    },
+  },
+  ],
 })
