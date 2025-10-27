01433fdf9352016fdd61679370d6ab4e9efd02e2
Lázár Zsolt <54175457+Zsolti145@users.noreply.github.com>
2025-10-27 20:41:39 +0100

    Update Success.jsx

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -5,6 +5,7 @@ function Success() {
       <p className="mb-4">Hamarosan felvesszük Önnel a kapcsolatot.</p>
       <a href="/" className="succes-btn ">Vissza a főoldalra</a>
     </section>
+    
   );
 }
 
