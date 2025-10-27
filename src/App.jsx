8d359e131e16d51b445da873ecd3792a43b5daf9
Lázár Zsolt <54175457+Zsolti145@users.noreply.github.com>
2025-10-27 20:58:53 +0100

    Comment out success route in App component
    Comment out the success route for testing purposes.

--- src/App.jsx
+++ src/App.jsx
@@ -92,8 +92,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/Success.html" element={<Success/>} />
-        {/* <Route path="/success" element={<Success/>} /> Teszt okból kilövöm hátha megy a submission*/}
+               {/* <Route path="/success" element={<Success/>} /> Teszt okból kilövöm hátha megy a submission*/}
       </Routes>
     </BrowserRouter>
   );
